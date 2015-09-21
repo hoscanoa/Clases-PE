2f9250abd2dda56ce28904d44a4f2f42920b8bdb
hoscanoa <hoscanoa@gmail.com>
2015-09-21 12:21:30 -0500

    Completo

---
+++ Semana4/src/cibertec/edu/pe/beans/UsuarioBean.java
@@ -0,0 +1,31 @@
+package cibertec.edu.pe.beans;
+
+public class UsuarioBean {
+	private String login;
+	private String password;
+	private String nombre;
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+}
---
+++ Semana4/src/cibertec/edu/pe/managed/UsuarioManaged.java
@@ -0,0 +1,27 @@
+package cibertec.edu.pe.managed;
+
+import javax.faces.bean.ManagedBean;
+
+import cibertec.edu.pe.beans.UsuarioBean;
+
+@ManagedBean(name = "usuarioManaged")
+public class UsuarioManaged {
+	private UsuarioBean usuario = new UsuarioBean();
+
+	public String validar() {
+		String ir = "/ui/denegado.jsf";
+		if (usuario.getLogin().equalsIgnoreCase("CIBERTEC") && usuario.getPassword().equals("123")) {
+			ir = "/ui/bienvenido.jsf";
+		}
+		return ir;
+	}
+
+	public UsuarioBean getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(UsuarioBean usuario) {
+		this.usuario = usuario;
+	}
+	
+}
